3886ab401130947449c6eedd278c7f99aa0c1b3f
Michael Schwartz <mykesx@gmail.com>
2015-02-21 08:03:10 -0800

    add base64_encode to support module

--- modules/support/index.js
+++ modules/support/index.js
@@ -7,9 +7,10 @@
 
 /*global require, module */
 module.exports = {
-    uuid: require('lib/uuid'),
-    md5: require('lib/md5'),
-	sha1: require('lib/sha1'),
-	utf8_encode: require('lib/utf8_encode'),
-    randomString: require('lib/randomString')
+    uuid          : require('lib/uuid'),
+    md5           : require('lib/md5'),
+    sha1          : require('lib/sha1'),
+    utf8_encode   : require('lib/utf8_encode'),
+    randomString  : require('lib/randomString'),
+    base64_encode : require('lib/base64_encode')
 };

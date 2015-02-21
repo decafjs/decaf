4d9787a7f3590ded64bbc6ae25e381454931b3ca
Michael Schwartz <mykesx@gmail.com>
2015-02-21 08:03:37 -0800

    Use uuid, base64_encode from support module

--- modules/http/lib/WebSocket.js
+++ modules/http/lib/WebSocket.js
@@ -7,210 +7,7 @@
 /*global require, java, sync */
 var GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11",
     {Thread} = require('Threads'),
-    sha1 = require('support').sha1;
-
-//function sha1 (str) {
-//    // http://kevin.vanzonneveld.net
-//    // +   original by: Webtoolkit.info (http://www.webtoolkit.info/)
-//    // + namespaced by: Michael White (http://getsprink.com)
-//    // +      input by: Brett Zamir (http://brett-zamir.me)
-//    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-//    // -    depends on: utf8_encode
-//    // *     example 1: sha1('Kevin van Zonneveld');
-//    // *     returns 1: '54916d2e62f65b3afa6e192e6a601cdbe5cb5897'
-//    var rotate_left = function (n, s) {
-//        var t4 = (n << s) | (n >>> (32 - s));
-//        return t4;
-//    };
-//
-//    /*var lsb_hex = function (val) { // Not in use; needed?
-//     var str="";
-//     var i;
-//     var vh;
-//     var vl;
-//
-//     for ( i=0; i<=6; i+=2 ) {
-//     vh = (val>>>(i*4+4))&0x0f;
-//     vl = (val>>>(i*4))&0x0f;
-//     str += vh.toString(16) + vl.toString(16);
-//     }
-//     return str;
-//     };*/
-//
-//    var cvt_hex = function (val) {
-//        var str = "";
-//        var i;
-//        var v;
-//
-//        for (i = 7; i >= 0; i--) {
-//            v = (val >>> (i * 4)) & 0x0f;
-//            str += v.toString(16);
-//        }
-//        return str;
-//    };
-//
-//    var blockstart;
-//    var i, j;
-//    var W = new Array(80);
-//    var H0 = 0x67452301;
-//    var H1 = 0xEFCDAB89;
-//    var H2 = 0x98BADCFE;
-//    var H3 = 0x10325476;
-//    var H4 = 0xC3D2E1F0;
-//    var A, B, C, D, E;
-//    var temp;
-//
-//    str = utf8_encode(str);
-//    var str_len = str.length;
-//
-//    var word_array = [];
-//    for (i = 0; i < str_len - 3; i += 4) {
-//        j = str.charCodeAt(i) << 24 | str.charCodeAt(i + 1) << 16 | str.charCodeAt(i + 2) << 8 | str.charCodeAt(i + 3);
-//        word_array.push(j);
-//    }
-//
-//    switch (str_len % 4) {
-//        case 0:
-//            i = 0x080000000;
-//            break;
-//        case 1:
-//            i = str.charCodeAt(str_len - 1) << 24 | 0x0800000;
-//            break;
-//        case 2:
-//            i = str.charCodeAt(str_len - 2) << 24 | str.charCodeAt(str_len - 1) << 16 | 0x08000;
-//            break;
-//        case 3:
-//            i = str.charCodeAt(str_len - 3) << 24 | str.charCodeAt(str_len - 2) << 16 | str.charCodeAt(str_len - 1) << 8 | 0x80;
-//            break;
-//    }
-//
-//    word_array.push(i);
-//
-//    while ((word_array.length % 16) != 14) {
-//        word_array.push(0);
-//    }
-//
-//    word_array.push(str_len >>> 29);
-//    word_array.push((str_len << 3) & 0x0ffffffff);
-//
-//    for (blockstart = 0; blockstart < word_array.length; blockstart += 16) {
-//        for (i = 0; i < 16; i++) {
-//            W[i] = word_array[blockstart + i];
-//        }
-//        for (i = 16; i <= 79; i++) {
-//            W[i] = rotate_left(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
-//        }
-//
-//
-//        A = H0;
-//        B = H1;
-//        C = H2;
-//        D = H3;
-//        E = H4;
-//
-//        for (i = 0; i <= 19; i++) {
-//            temp = (rotate_left(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
-//            E = D;
-//            D = C;
-//            C = rotate_left(B, 30);
-//            B = A;
-//            A = temp;
-//        }
-//
-//        for (i = 20; i <= 39; i++) {
-//            temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
-//            E = D;
-//            D = C;
-//            C = rotate_left(B, 30);
-//            B = A;
-//            A = temp;
-//        }
-//
-//        for (i = 40; i <= 59; i++) {
-//            temp = (rotate_left(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
-//            E = D;
-//            D = C;
-//            C = rotate_left(B, 30);
-//            B = A;
-//            A = temp;
-//        }
-//
-//        for (i = 60; i <= 79; i++) {
-//            temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
-//            E = D;
-//            D = C;
-//            C = rotate_left(B, 30);
-//            B = A;
-//            A = temp;
-//        }
-//
-//        H0 = (H0 + A) & 0x0ffffffff;
-//        H1 = (H1 + B) & 0x0ffffffff;
-//        H2 = (H2 + C) & 0x0ffffffff;
-//        H3 = (H3 + D) & 0x0ffffffff;
-//        H4 = (H4 + E) & 0x0ffffffff;
-//    }
-//
-//    temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
-//    return temp.toLowerCase();
-//}
-
-function base64_encode (data) {
-    // http://kevin.vanzonneveld.net
-    // +   original by: Tyler Akins (http://rumkin.com)
-    // +   improved by: Bayron Guevara
-    // +   improved by: Thunder.m
-    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   bugfixed by: Pellentesque Malesuada
-    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   improved by: Rafał Kukawski (http://kukawski.pl)
-    // *     example 1: base64_encode('Kevin van Zonneveld');
-    // *     returns 1: 'S2V2aW4gdmFuIFpvbm5ldmVsZA=='
-    // mozilla has this native
-    // - but breaks in 2.0.0.12!
-    //if (typeof this.window['btoa'] == 'function') {
-    //    return btoa(data);
-    //}
-    var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-    var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
-        ac = 0,
-        enc = "",
-        tmp_arr = [];
-
-    if (!data) {
-        return data;
-    }
-
-    do { // pack three octets into four hexets
-        o1 = data.charCodeAt(i++);
-        o2 = data.charCodeAt(i++);
-        o3 = data.charCodeAt(i++);
-
-        bits = o1 << 16 | o2 << 8 | o3;
-
-        h1 = bits >> 18 & 0x3f;
-        h2 = bits >> 12 & 0x3f;
-        h3 = bits >> 6 & 0x3f;
-        h4 = bits & 0x3f;
-
-        // use hexets to index into b64, and append result to encoded string
-        tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
-    } while (i < data.length);
-
-    enc = tmp_arr.join('');
-
-    var r = data.length % 3;
-
-    return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
-
-}
-
-function uuid() {
-    function s4() {
-        return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-    }
-    return (s4()+s4()+"-"+s4()+"-"+s4()+"-"+s4()+"-"+s4()+s4()+s4());
-}
+    {sha1, base64_encode, uuid} = require('support');
 
 
 function makeAccept(key) {
@@ -244,9 +41,9 @@ var webSockets = {};
 function WebSocket(req, res) {
     res.status = 101;
     decaf.extend(res.headers, {
-        'Upgrade'             : 'websocket',
-        'Connection'          : 'upgrade',
-        'Sec-WebSocket-Accept': makeAccept(req.headers['sec-websocket-key']) // base64_encode(buf)
+        'Upgrade'              : 'websocket',
+        'Connection'           : 'upgrade',
+        'Sec-WebSocket-Accept' : makeAccept(req.headers['sec-websocket-key']) // base64_encode(buf)
     });
     res.sendHeaders();
     res.flush();
@@ -286,7 +83,7 @@ decaf.extend(WebSocket.prototype, {
      * @param {string} eventName name of event
      * @param {function} handler function to call when event is fired
      */
-    on: function (eventName, handler) {
+    on : function (eventName, handler) {
         this.handlers[eventName] = this.handlers[eventName] || [];
         this.handlers[eventName].push(handler);
     },
@@ -300,7 +97,7 @@ decaf.extend(WebSocket.prototype, {
      * @param {string} eventName name/kind of event to fire
      * @param {object} data arbitrary data passed to event handlers
      */
-    fireEvent: function (eventName, data) {
+    fireEvent : function (eventName, data) {
         var handlers = this.handlers[eventName];
         if (handlers) {
             decaf.each(handlers, function (handler) {
@@ -318,7 +115,7 @@ decaf.extend(WebSocket.prototype, {
      * @param {string} path WebSocket path
      * @param {string} message message to send
      */
-    broadcast: function (path, message) {
+    broadcast : function (path, message) {
         var me = this;
         new Thread(function () {
             decaf.each(webSockets, function (ws) {
@@ -329,7 +126,7 @@ decaf.extend(WebSocket.prototype, {
         }).start();
     },
 
-    run: function () {
+    run : function () {
         var is = this.req.is,
             message;
 
@@ -345,7 +142,7 @@ decaf.extend(WebSocket.prototype, {
         delete webSockets[this.uuid];
     },
 
-    rawSendMessage: function (s) {
+    rawSendMessage : function (s) {
         var os = this.res.os.socket.getOutputStream(),
             len = s.length;
 
@@ -373,19 +170,19 @@ decaf.extend(WebSocket.prototype, {
         os.flush();
     },
 
-    rawSendPing: function () {
+    rawSendPing : function () {
         var os = this.res.os.socket.getOutputStream();
         os.write(0x89);
         os.write(0);
     },
 
-    rawSendPong: function () {
+    rawSendPong : function () {
         var os = this.res.os.socket.getOutputStream();
         os.write(0x8a);
         os.write(0);
     },
 
-    getMessage: function (is) {
+    getMessage : function (is) {
         function next() {
             return is.readByte();
         }
@@ -470,5 +267,5 @@ decaf.extend(WebSocket.prototype, {
 });
 
 decaf.extend(exports, {
-    WebSocket: WebSocket
+    WebSocket : WebSocket
 });

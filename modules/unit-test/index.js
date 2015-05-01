ab40cb923431d1f66fdadb14deb3cae113541f61
Michael Schwartz <mykesx@gmail.com>
2015-05-01 10:15:48 -0700

    unit test use require() instead of Function to load test suite

--- modules/unit-test/index.js
+++ modules/unit-test/index.js
@@ -67,8 +67,7 @@ function test_main(path) {
         dir  = new File(path);
 
     decaf.each(dir.list(/\.js$/), function (filename) {
-        var fn = require((path + '/' + filename).replace(/\/\//g)); // new Function(new File((path + '/' + filename).replace(/\/\//g, '/')).readAll());
-        fn();
+        require((path + '/' + filename).replace(/\/\//g)); // new Function(new File((path + '/' + filename).replace(/\/\//g, '/')).readAll());
     });
 
     if (global.arguments.length > 1) {

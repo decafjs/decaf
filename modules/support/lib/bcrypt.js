2d38402542116f1ce7c5d217b4c0a4a0cd708288
Michael Schwartz <mykesx@gmail.com>
2015-06-01 09:42:07 -0700

    Make backward compatible, fixed.

--- modules/support/lib/bcrypt.js
+++ modules/support/lib/bcrypt.js
@@ -25,8 +25,12 @@ var JBCrypt = Packages.org.mindrot.jbcrypt.BCrypt;
 decaf.extend(exports, {
     BCrypt     : JBCrypt,       // for backward compatibility
     saltLength : 12,
-    hashpw     : function (password) {
-        return String(JBCrypt.hashpw(password, JBCrypt.gensalt(this.saltLength)));
+    hashpw     : function (password, salt) {
+        salt = salt || JBCrypt.gensalt(this.saltLength);
+        return String(JBCrypt.hashpw(password, salt));
+    },
+    gensalt    : function (len) {
+        return String(JBCrypt.gensalt(len));
     },
     compare    : function (candidate, hashed) {
         return !!JBCrypt.checkpw(candidate, hashed);

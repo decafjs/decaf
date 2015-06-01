3f07838816adc56dca50e1ef3465a0b6754fc08b
Michael Schwartz <mykesx@gmail.com>
2015-06-01 09:34:42 -0700

    Cleanup code

--- modules/support/lib/bcrypt.js
+++ modules/support/lib/bcrypt.js
@@ -23,12 +23,12 @@
 var JBCrypt = Packages.org.mindrot.jbcrypt.BCrypt;
 
 decaf.extend(exports, {
-    BCrypt : JBCrypt,
-    saltLength: 12,
-    hashpw: function(password) {
+    BCrypt     : JBCrypt,       // for backward compatibility
+    saltLength : 12,
+    hashpw     : function (password) {
         return String(JBCrypt.hashpw(password, JBCrypt.gensalt(this.saltLength)));
     },
-    compare: function(candidate, hashed) {
+    compare    : function (candidate, hashed) {
         return !!JBCrypt.checkpw(candidate, hashed);
     }
 });
